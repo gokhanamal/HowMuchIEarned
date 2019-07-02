a83a796b0dd0cb0fdf13e1a99825dd4694d76ddf
gokhanamal <gokhanamal@gmail.com>
2019-07-02 22:08:33 +0300

    Initial Commit

---
+++ HowMuchIEarned/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HowMuchIEarned
+//
+//  Created by Gokhan Namal on 2.07.2019.
+//  Copyright © 2019 Gokhan Namal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HowMuchIEarned/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HowMuchIEarned
+//
+//  Created by Gokhan Namal on 2.07.2019.
+//  Copyright © 2019 Gokhan Namal. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
